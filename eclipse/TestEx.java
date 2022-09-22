5e794c726def02da7d69054ed58e73c6d17e180c
HanGysSik <gkfrbtlr1357@naver.com>
2022-09-22 14:16:34 +0900

    asd

--- eclipse/TestEx.java
+++ eclipse/TestEx.java
@@ -4,16 +4,24 @@ public class TestEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int RA = 4;
-		for(int i=2; i<10; i++) {
-			for(int j=1; j<10; j++) {
-				for(int k=0; k<RA; k++) {
-					System.out.printf("%d*%d=%d\n", (i+k),j,(i+k)*j);
+//		for (int j = 1; j < 10; j++) {
+//			for (int i = 2; i < 10; i++) {
+//				System.out.printf("%2d*%2d=%2d\t\t", i, j, i*j);
+//			}
+//			System.out.print("\n");
+//		}
+		int dan = 3;
+		for (int i = 2; i < 10; i+=dan) {
+			for (int j = 1; j < 10; j++) {
+				for (int k = 0 ; k < dan ; k++) {
 					
+					System.out.printf("%d*%d=%d", (i+k), j, (i+k)*j);
+					
+					if (k < dan - 1)	System.out.print("\t");
+					else				System.out.print("\n");
 				}
 			}
+			System.out.print("\n");
 		}
-
 	}
-
 }
---
+++ eclipse/TestGugudan.java
@@ -0,0 +1,12 @@
+package com.rubyu.java.test1;
+
+public class TestGugudan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gugudan gg = new Gugudan();
+		gg.printGugudan(5);
+
+	}
+
+}
